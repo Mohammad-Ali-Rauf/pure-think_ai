56eed72ae0e165a706cf75b75d446754e5ae0d1a
Mohammad-Ali-Rauf <m.aliadnanrauf@gmail.com>
2024-01-04 01:19:35 +0500

    completed github auth and added google button only

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,12 +1,41 @@
+'use client'
 import { Button } from '@/components/ui/button'
-import React from 'react'
+import { signOut, useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
 
 type Props = {}
 
 const Dashboard = (props: Props) => {
-  return (
-    <div><Button size="lg" variant="secondary">Click Me</Button></div>
-  )
+	const { data: session, status } = useSession()
+	const { push } = useRouter()
+	const [loading, setLoading] = useState(false)
+
+	useEffect(() => {
+		if (status === 'unauthenticated' && !session && !loading) {
+      setLoading(true)
+			push('/sign-in')
+      setLoading(false)
+		} else {
+			setLoading(false)
+		}
+	}, [session, status, push, loading])
+
+	if (loading) {
+		return (
+			<div className='absolute right-1/2 bottom-1/2  transform translate-x-1/2 translate-y-1/2 '>
+				<div className='border-t-transparent border-solid animate-spin  rounded-full border-blue-400 border-8 h-64 w-64'></div>
+			</div>
+		)
+	} else {
+		return (
+			<div>
+				<Button onClick={() => signOut()} size='lg' variant='secondary'>
+					sign out
+				</Button>
+			</div>
+		)
+	}
 }
 
 export default Dashboard
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,27 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import SessionProvider from '@/components/SessionProvider'
+import { getServerSession } from 'next-auth'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'PureThink AI',
-  description: 'Created, designed, and developed by Mohammad Ali',
+	title: 'PureThink AI',
+	description: 'Created, designed, and developed by Mohammad Ali',
 }
 
-export default function RootLayout({
-  children,
+export default async function RootLayout({
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+	const session = await getServerSession()
+	return (
+		<html lang='en'>
+			<body className={inter.className}>
+				<SessionProvider session={session}>{children}</SessionProvider>
+			</body>
+		</html>
+	)
 }
