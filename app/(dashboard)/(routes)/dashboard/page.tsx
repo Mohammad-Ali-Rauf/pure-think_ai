28a6cb91c0117999ece2b7d7b54de9baa4f362c1
Mohammad-Ali-Rauf <m.aliadnanrauf@gmail.com>
2024-01-03 09:52:46 +0500

    setup basic folder structure and shadcn/ui

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+type Props = {}
+
+const Dashboard = (props: Props) => {
+  return (
+    <div><Button size="lg" variant="secondary">Click Me</Button></div>
+  )
+}
+
+export default Dashboard
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+type Props = {}
+
+const LandingPage = (props: Props) => {
+  return (
+    <div>Landing Page</div>
+  )
+}
+
+export default LandingPage
